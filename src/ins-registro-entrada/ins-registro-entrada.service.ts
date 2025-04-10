52208cee5549dda991f60b57a3d94d772c299719
Registro Vehicular <vehicularregistro526@gmail.com>
2025-04-10 14:13:37 -0500

    Update ins-registro-entrada.service.ts - Se modifico el formato de fecha

--- src/ins-registro-entrada/ins-registro-entrada.service.ts
+++ src/ins-registro-entrada/ins-registro-entrada.service.ts
@@ -489,7 +489,7 @@ export class InsRegistroEntradaService {
 
       const pdfBuffer: Buffer = await this.exportSheetAsPDF(spreadsheetrev3);
       const sucursal1 = nameText.sucursal.match(/\((.*?)\)/)?.[1] || '';
-      const fechaSinGuiones = nameText.fecha.replace(/-/g, '').replace(', ', '').replace(':', '');
+      const fechaSinGuiones = nameText.fecha.replace(/-/g, '').replace(', ', '').replace(':', '').slice(4, 6) + nameText.fecha.slice(6, 8) + nameText.fecha.slice(0, 4);
       const originalname = `${fechaSinGuiones}-${sucursal1}-${nameText.placa}-R06-PT-19-Revisión de Vehículos-${nuevoNumero}`;
 
       await this.uploadFileToDrive({
