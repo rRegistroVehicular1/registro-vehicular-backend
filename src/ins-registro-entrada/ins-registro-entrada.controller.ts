c811ddefb7eb161d31e03c098833c8b026e2efd9
Registro Vehicular <vehicularregistro526@gmail.com>
2025-05-09 08:55:59 -0500

    Update ins-registro-entrada.controller.ts

--- src/ins-registro-entrada/ins-registro-entrada.controller.ts
+++ src/ins-registro-entrada/ins-registro-entrada.controller.ts
@@ -15,13 +15,18 @@ export class InsRegistroEntradaController {
     return { lastOdometro };
   }
   
-  @Post('register')
+  @Post('register')  
   @UseInterceptors(FileInterceptor('documento'))
   async register(
     @Body() body: any
   ) {
 
-    const {revisiones, observacion, lastPlacaInfo, odometro } = body;
+    const {revisiones, observacion, lastPlacaInfo, odometro, placa } = body;
+
+    const lastOdometro = await this.insRegistroEntradaService.getLastOdometro(placa);
+    if (parseFloat(odometro) <= lastOdometro) {
+      throw new BadRequestException(`El odómetro debe ser mayor al último registrado (${lastOdometro})`);
+    }
     const result = await this.insRegistroEntradaService.processRegistroEntrada(revisiones, observacion, lastPlacaInfo, odometro);
     return result;
   }
