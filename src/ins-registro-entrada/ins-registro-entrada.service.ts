ee19e19072b674b63cd574eca1e49e125249e972
Registro Vehicular <vehicularregistro526@gmail.com>
2025-05-11 19:10:14 -0500

    Update ins-registro-entrada.service.ts

--- src/ins-registro-entrada/ins-registro-entrada.service.ts
+++ src/ins-registro-entrada/ins-registro-entrada.service.ts
@@ -195,8 +195,8 @@ export class InsRegistroEntradaService {
       const tipoVehiculo = row[0][4];
       const odometro = row[0][5];
       const horas = row[0]; //.slice(-2);
-      const HoraSalida = row[0][188]; //GF
-      const HoraEntrada = row[0][189]; //GG
+      const HoraSalida = row[0][187]; //GF
+      const HoraEntrada = row[0][188]; //GG
 
       const nuevoNumero = await this.generarNumeroConsecutivo(sucursal);
 
@@ -330,7 +330,7 @@ export class InsRegistroEntradaService {
       const revisionCuñas = row[0][185];
 
       const observacionGeneralDatos = row[0][186];
-      const odometroEntrada = row[0][190];
+      const odometroEntrada = row[0][189];
 
       const requests = [
 
