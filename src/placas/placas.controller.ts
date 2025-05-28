67f1cee4f87bff195345718a0af8d502509b7cb2
Registro Vehicular <vehicularregistro526@gmail.com>
2025-05-28 08:29:45 -0500

    Update placas.controller.ts --> vinculando tipo vehiculo con placa

--- src/placas/placas.controller.ts
+++ src/placas/placas.controller.ts
@@ -7,6 +7,18 @@ import { ApiTags } from '@nestjs/swagger';
 export class PlacasController {
   constructor(private readonly placasService: PlacasService) { }
 
+  // placas.controller.ts (backend)
+  @Get('get-placas-y-tipos')
+  async getPlacasYTipos() {
+      try {
+          const data = await this.placasService.getPlacasYTipoFromSheet();
+          return data;
+      } catch (error) {
+          console.error('Error en controller:', error);
+          return []; // Siempre devuelve array aunque falle
+      }
+  }
+  
   @Get('get-data-placas')
   async getData() {
     try {
