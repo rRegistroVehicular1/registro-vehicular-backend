968646cba620586d7699a36b640e131eef38b452
Registro Vehicular <vehicularregistro526@gmail.com>
2025-05-28 15:21:52 -0500

    Update placas.controller.ts --> vinculando tipo vehiculo con placa

--- src/placas/placas.controller.ts
+++ src/placas/placas.controller.ts
@@ -6,20 +6,8 @@ import { ApiTags } from '@nestjs/swagger';
 @Controller('placas')
 export class PlacasController {
   constructor(private readonly placasService: PlacasService) { }
-
-  // placas.controller.ts (backend)
-  @Get('get-placas-y-tipos')
-  async getPlacasYTipos() {
-      try {
-          const data = await this.placasService.getPlacasYTipoFromSheet();
-          return data;
-      } catch (error) {
-          console.error('Error en controller:', error);
-          return []; // Siempre devuelve array aunque falle
-      }
-  }
   
-  {/*@Get('get-data-placas')
+  @Get('get-data-placas')
   async getData() {
     try {
       const data = await this.placasService.getPlacasFromSheet();
@@ -28,7 +16,7 @@ export class PlacasController {
       console.error('Error en controller:', error);
       return []; // Siempre devuelve array aunque falle
     }
-  }*/}
+  }
 
   @Get('get-vehiculos')
   async getVehiculos() {
@@ -38,6 +26,17 @@ export class PlacasController {
       } catch (error) {
           console.error('Error en controller:', error);
           return {};
+      }
+  }
+
+  @Get('get-placas-y-tipos')
+  async getPlacasYTipos() {
+      try {
+          const data = await this.placasService.getPlacasYTipoFromSheet();
+          return data;
+      } catch (error) {
+          console.error('Error en controller:', error);
+          return {}; // Siempre devuelve array aunque falle
       }
   }
 }
