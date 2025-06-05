c5e485a2399608d0bc3d2e401fa8b01094d7bdd6
Registro Vehicular <vehicularregistro526@gmail.com>
2025-06-05 15:39:39 -0500

    Update placas.controller.ts --> version original

--- src/placas/placas.controller.ts
+++ src/placas/placas.controller.ts
@@ -18,17 +18,6 @@ export class PlacasController {
     }
   }
 
-  @Get('get-cantidad-llantas')
-  async getCantidadLlantas() {
-      try {
-          const data = await this.placasService.getCantidadLlantas();
-          return data;
-      } catch (error) {
-          console.error('Error en controller:', error);
-          return {};
-      }
-  }
-  
   @Get('get-vehiculos')
   async getVehiculos() {
       try {
