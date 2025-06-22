3a419cf0eca2082a3fb99f678652ebef2f875a3f
Registro Vehicular <vehicularregistro526@gmail.com>
2025-06-22 00:42:21 -0500

    Update ins-registro-salida.module.ts

--- src/ins-registro-salida/ins-registro-salida.module.ts
+++ src/ins-registro-salida/ins-registro-salida.module.ts
@@ -3,9 +3,14 @@ import { InsRegistroSalidaService } from './ins-registro-salida.service';
 import { InsRegistroSalidaController } from './ins-registro-salida.controller';
 import { AppModule } from 'src/app.module';
 import { SalidasModule } from 'src/salidas/salidas.module';
+import { PlacasModule } from '../placas/placas.module';
 
 @Module({
-  imports: [forwardRef(() => AppModule),SalidasModule],
+  imports: [
+    forwardRef(() => AppModule),
+    forwardRef(() => SalidasModule),
+    forwardRef(() => PlacasModule)
+  ],
   controllers: [InsRegistroSalidaController],
   providers: [InsRegistroSalidaService],
 })
